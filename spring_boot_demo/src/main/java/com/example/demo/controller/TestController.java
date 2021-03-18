f938f29ee46a8d72de2452c31dd03e6568742e69
LiuFuMing <421619670@qq.com>
2021-03-18 18:18:35 +0800

    第六次提交

--- spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
+++ spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
@@ -13,7 +13,7 @@ import com.example.demo.model.Test;
 public class TestController {
 	
 
-    private static final String template = "这个第五次修改，Hello, %s!";
+    private static final String template = "这个第六次修改，Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/test")
