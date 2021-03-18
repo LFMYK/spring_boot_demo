4d9c14f0e60b109bdda43b97786819c64ee126b9
LiuFuMing <421619670@qq.com>
2021-03-18 17:45:19 +0800

    # WARNING: head commit changed in the meantime
    第一次提交

--- spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
+++ spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
@@ -10,7 +10,7 @@ import com.example.demo.model.Test;
 @RestController
 public class TestController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "这个第三次修改，Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/test")
