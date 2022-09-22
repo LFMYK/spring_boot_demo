4ed72871ad65de17a4053d08f89539857da8ea49
LFMYK <421619670@qq.com>
2022-09-22 10:21:50 +0800

    第十二次提交

--- spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
+++ spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
@@ -13,7 +13,7 @@ import com.example.demo.model.Test;
 public class TestController {
 	
 
-    private static final String template = "这个第12次修改，Hello, %s!";
+    private static final String template = "这个第13次修改，Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/test")
