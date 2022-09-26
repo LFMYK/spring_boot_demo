690229c7c93876d073dc78063a852d018febe904
LFMYK <421619670@qq.com>
2022-09-27 04:04:09 +0800

    第二十次提交！

--- spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
+++ spring_boot_demo/src/main/java/com/example/demo/controller/TestController.java
@@ -13,7 +13,7 @@ import com.example.demo.model.Test;
 public class TestController {
 	
 
-    private static final String template = "这个第19次修改，Hello, %s!";
+    private static final String template = "这个第20次修改，Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/test")
